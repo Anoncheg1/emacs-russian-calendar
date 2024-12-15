c5b67276b4921e5fef0406b9a08664018fe1e02d
none <none>
2024-12-15 15:06:26 +0000

    0.0.6

--- russian-calendar-tests.el
+++ russian-calendar-tests.el
@@ -4,7 +4,7 @@
 ;; Author: github.com/Anoncheg1,codeberg.org/Anoncheg
 ;; Keywords: calendar, holidays
 ;; URL: https://github.com/Anoncheg1/emacs-russian-calendar
-;; Version: 0.0.5
+;; Version: 0.0.6
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
